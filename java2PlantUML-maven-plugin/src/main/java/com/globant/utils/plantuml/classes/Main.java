0f822cf9510252d44506727c825a74f755b8d0c9
efrain.calla <ecallac@gmail.com>
2017-02-24 10:36:37 -0500

    # WARNING: head commit changed in the meantime

--- java2PlantUML-maven-plugin/src/main/java/com/globant/utils/plantuml/classes/Main.java
+++ java2PlantUML-maven-plugin/src/main/java/com/globant/utils/plantuml/classes/Main.java
@@ -13,12 +13,14 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         String thePackage = 1 == args.length ? args[0] : "com.globant.utils.plantuml.classes.beans,com.globant.utils.plantuml.classes.render";
         System.out.println("printing this.");
+        String path = "C:\\Bluestarenergy\\git\\maven.1486422738603\\java2PlantUML-maven-plugin";
+//        String path = "C:\\Bluestarenergy\\git\\maven.1486422738603\\java2PlantUML-maven-plugin";
         Parser.parse(
                 thePackage, 
                 Filters.FILTER_CHAIN_RELATION_TYPE_STANDARD, 
                 Filters.FILTER_CHAIN_CLASSES_STANDARD,
                 Filters.FILTER_CHAIN_RELATION_STANDARD,
                 null,
-                "D:/git/java2PlantUML-maven-plugin",Constants.RENDER_TYPE_CLASS_BY_CLASS);
+                path,Constants.RENDER_TYPE_CLASS_BY_CLASS);
     }
 }
